56c7d7972c0c67141c5bf1ea50ee1f9b8f4582de
Antônio Neto <antonio_02neto@hotmail.com>
2024-03-08 12:06:02 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(document).ready(function() {
+        $('#Telefone').mask('(00) 00000-0000')
+        $('#CPF').mask('000.000.000-00')
+        $('#CEP').mask('00.000-000')
+    })
+})
+
